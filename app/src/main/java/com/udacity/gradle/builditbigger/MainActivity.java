4850f047c55dd8faf9ac3c46957e551c72d8a95c
Peter Arcentales <peterarcentales@gmail.com>
2015-11-30 17:18:56 -0500

    Completed Step 0 and 1

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.andressantibanez.Joker;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joker joker = new Joker();
+        Toast.makeText(this, joker.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ joker/src/main/java/com/andressantibanez/Joker.java
@@ -0,0 +1,13 @@
+package com.andressantibanez;
+
+public class Joker {
+
+    public Joker() {
+
+    }
+
+    public String getJoke() {
+        return "Very funny joke!";
+    }
+
+}
