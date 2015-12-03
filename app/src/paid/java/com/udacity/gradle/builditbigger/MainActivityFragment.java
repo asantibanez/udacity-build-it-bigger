d3bec1a7be897b50c982f113a50deb6fe9138437
Peter Arcentales <peterarcentales@gmail.com>
2015-12-03 15:41:27 -0500

    Fixes according to first review

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,31 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        boolean isAppPaidVersion = getResources().getBoolean(R.bool.is_paid_version);
+        Log.d(LOG_TAG, "Am I the paid version?: " + isAppPaidVersion);
+
+        return root;
+    }
+}
